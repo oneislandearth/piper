ea852a048b31f52b58af207333e86f1dc6eea418
Daniel Larkin <daniel@larkin.nz>
2022-11-25 00:09:36 +1300

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Download the config file
-curl -qks "https://raw.githubusercontent.com/oneislandearth/piper/main/ngrok.service?token=GHSAT0AAAAAABYHH5REQ7ML34XOP5AGFP5AY37LLKA" \
-| sed "s/\$username/$(whoami)/igm" > ngrok2.service
+curl -qks \
+"https://raw.githubusercontent.com/oneislandearth/piper/main/ngrok.service?token=GHSAT0AAAAAABYHH5REFL3QQUGGI7KE5THKY37L3OA" \
+| sed "s/\$username/$(whoami)/igm" \
+> ngrok2.service
