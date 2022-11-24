48e08e2279deb1b5bf3c0407ec53a76e85132730
Daniel Larkin <daniel@larkin.nz>
2022-11-25 00:15:36 +1300

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -5,3 +5,10 @@ curl -qks \
 "https://raw.githubusercontent.com/oneislandearth/piper/main/ngrok.service?token=GHSAT0AAAAAABYHH5REFL3QQUGGI7KE5THKY37L3OA" \
 | sed "s/\$username/$(whoami)/igm" \
 > ngrok2.service
+
+# 
+curl -qks \
+"https://raw.githubusercontent.com/oneislandearth/piper/main/ngrok.yml?token=GHSAT0AAAAAABYHH5RFBZ4DOCPVIOIJLTHWY37L6NA" \
+| sed "s/\$token/$1/igm" \
+| sed "s/\$domain/$2/igm" \
+> ngrok2.yml
